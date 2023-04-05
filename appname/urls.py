f105bf8fe8780ad4ef5fa20a7fea4757851afa4a
Sripriya Simhadri <simhadrisripriya@Sripriyas-MacBook-Air.local>
2023-04-05 16:41:30 -0400

    events

--- appname/urls.py
+++ appname/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('account/', views.account, name='account'),
     path('signup/', views.signup, name='signup'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('events1/', views.events1, name='events1'),
+    path('events2/', views.events2, name='events2'),
+    path('events3/', views.events3, name='events3'),
 ]
